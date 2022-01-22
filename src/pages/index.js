5531bdc778d64aa847f2f1370527896225acf5cb
Eivind Gussiås Løkseth <eivind@outlook.com>
2022-01-22 14:07:34 +0100

    Use Gatsby plugins

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import * as React from "react";
 import Layout from '../components/layout';
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home">
       <p>This is the Gatsby Tutorial.</p>
+      <StaticImage  alt="My top nine on Instagram 2020"
+        src="../images/topnine2020.jpg" />
     </Layout>
   );
 }
