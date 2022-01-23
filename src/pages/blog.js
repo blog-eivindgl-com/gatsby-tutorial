8b51ef874a6df7b963f0f5e24213298e663072c8
Eivind Gussiås Løkseth <eivind@outlook.com>
2022-01-23 22:55:56 +0100

    MDX rendering

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,17 +1,22 @@
 import { graphql } from 'gatsby';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
 import * as React from 'react';
 import Layout from '../components/layout';
 
 const BlogPage = ({data}) => {
     return (
         <Layout pageTitle='Blog'>
-            <ul>
-            {
-                data.allFile.nodes.map(node => (
-                <li key={node.name}>{node.name}</li>
-                ))
-            }
-            </ul>
+        {
+            data.allMdx.nodes.map(node => (
+            <article key={node.id}>
+                <h2>{node.frontmatter.title}</h2>
+                <p>Posted: {node.frontmatter.date}</p>
+                <MDXRenderer>
+                    {node.body}
+                </MDXRenderer>
+            </article>
+            ))
+        }
         </Layout>
     );
 }
@@ -20,11 +25,15 @@ export default BlogPage;
 
 export const query = graphql`
 query {
-    allFile {
-      nodes {
-        name
-        sourceInstanceName
-      }
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+        nodes {
+            frontmatter {
+                date(formatString: "DD.MM.yyyy")
+                title
+            }
+            id
+            body
+        }
     }
-  }
+}
 `
